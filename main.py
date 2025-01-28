2fbed3158b89b118c45f00410c32cd5a5e2b81e8
Saranjith <saranvlmna@gmail.com>
2025-01-28 15:20:02 +0530

    update and delete

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class User(BaseModel):
     email: str
     password: str
 
+class UserUpdate(BaseModel):
+    new_email: str 
+
 @app.post("/users/")
 def create_user(user:User, db: Session = Depends(get_db)):
     return crud.create_user(db, user.username, user.email, user.password)
@@ -21,17 +24,15 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 @app.get("/users/username/{username}")
 def read_user_by_username(username: str, db: Session = Depends(get_db)):
-    print("get by username")
     return crud.get_user_by_username(db, username)
 
 @app.put("/users/{user_id}")
-def update_user_email(user_id: int, new_email: str, db: Session = Depends(get_db)):
-    print("put")
-    return crud.update_user_email(db, user_id, new_email)
+def update_user_email(user_id:int,email:UserUpdate,db: Session = Depends(get_db)):
+    print("update",db, user_id, email.new_email)
+    return crud.update_user_email(db, user_id, email.new_email)
 
 @app.delete("/users/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
-    print("delete")
     return crud.delete_user(db, user_id)
     
 
