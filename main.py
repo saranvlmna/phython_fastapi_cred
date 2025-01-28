bb300506079743f08cc4b601d5638b4c7d87d67e
Saranjith <saranvlmna@gmail.com>
2025-01-28 15:33:34 +0530

    router update

--- main.py
+++ main.py
@@ -14,24 +14,24 @@ class User(BaseModel):
 class UserUpdate(BaseModel):
     new_email: str 
 
-@app.post("/users/")
+@app.post("/user/")
 def create_user(user:User, db: Session = Depends(get_db)):
     return crud.create_user(db, user.username, user.email, user.password)
 
-@app.get("/users/{user_id}")
+@app.get("/user/{user_id}")
 def read_user(user_id: int, db: Session = Depends(get_db)):
     return crud.get_user(db, user_id)
 
-@app.get("/users/username/{username}")
+@app.get("/user/username/{username}")
 def read_user_by_username(username: str, db: Session = Depends(get_db)):
     return crud.get_user_by_username(db, username)
 
-@app.put("/users/{user_id}")
+@app.put("/user/{user_id}")
 def update_user_email(user_id:int,email:UserUpdate,db: Session = Depends(get_db)):
     print("update",db, user_id, email.new_email)
     return crud.update_user_email(db, user_id, email.new_email)
 
-@app.delete("/users/{user_id}")
+@app.delete("/user/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     return crud.delete_user(db, user_id)
     
